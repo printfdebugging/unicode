0f833ff209a00581dd02733cb8f6d93b043977a6
Sahil Gautam <printfdebugging@gmail.com>
2026-07-21 21:51:27 +0530

    initial project setup
    - create empty source files
    - create test files
    - add .clang-format
    - create pkgconfig file template
    - create tests

---
+++ include/unicode/unicode.h
@@ -0,0 +1,14 @@
+#ifndef UNICODE_H
+#define UNICODE_H
+
+#include <stdint.h>
+
+typedef uint_least8_t u8;
+typedef uint_least16_t u16;
+typedef uint_least32_t u32;
+typedef uint_least64_t u64;
+
+typedef u32 rune;
+typedef u8 byte;
+
+#endif
